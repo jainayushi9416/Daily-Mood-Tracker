ef060b44b5c29693314f9683732c769b3488fbd8
ayushijain9 <jainayushi9416@gmail.com>
2025-03-20 08:53:55 +0530

    commit

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-function saveMood(mood){
-    //console.log(mood);
-    let date = new Date();
-    //console.log(date);
-    localStorage.setItem(date, mood);
 
+window.addEventListener("load", (event) => {
+    document.getElementById('todayMood').innerHTML ="Today's Mood is:" + localStorage.getItem((new Date()).toDateString());
+  });
+  
+
+function saveMood(mood){
+    //console.log(mood.innerText);
+    let date=(new Date()).toDateString();
+    localStorage.setItem(date, mood.innerText);
+    document.getElementById('todayMood').innerHTML ="Today's Mood is:" + localStorage.getItem(date);
 }
 
 function displayData(){
+    document.getElementById("display").innerHTML ="";
     for(let i=0;i<window.localStorage.length;i++){
         let key = localStorage.key(i);
         let value = localStorage.getItem(key);
         let b =key+value;
         document.getElementById("display").innerHTML+= b;
     }
-   
+  
 }
